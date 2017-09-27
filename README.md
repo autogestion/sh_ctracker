1d0e0e437689cf310b8ae0ef82688f06076eb862
mk <nozdre@gmail.com>
2017-09-27 17:35:50 +0300

    packaging

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
-# Corruption tracker module for [Socialhome](https://github.com/jaywink/socialhome)
+# A federated Corruption Tracker
 
+Corruption tracker module for [Socialhome](https://github.com/jaywink/socialhome)
 Based on stand-alone [Corruption tracker](https://github.com/autogestion/corruption_tracker) engine
 
+Corruption tracker lets you make cases of corruption and professionally unfit of civil servant public and as result track the level of it in public institutions
+
 FB - https://www.facebook.com/activecorruptiontracking/
+
+
+Quick start
+-----------
+1. Add "ctracker" to your INSTALLED_APPS setting like this::
+
+    INSTALLED_APPS = [
+        ...
+        'ctracker',
+    ]
+
+2. Include the ctracker URLconf in socialhome project urls.py like this::
+
+    url(r'^ctracker/', include('ctracker.urls')),
+
+3. Run `python manage.py migrate` to create the ctracker models.
---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='sh_ctracker',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='BSD 3-Clause License',  # example license
+    description='A federated Corruption Tracker',
+    long_description=README,
+    url='',
+    author='Mykhailo Kushchenko',
+    author_email='',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.10',  # replace "X.Y" as appropriate
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD 3-Clause License',  # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # Replace these appropriately if you are stuck on Python 2.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
